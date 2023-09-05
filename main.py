01faa402f46232bd7f99716c70b6108ec0d02135
Victor Henrique Hardt <hardt.victor@gmail.com>
2023-09-04 21:40:53 -0300

    fix: success print

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from nodo import Nodo
+import asyncio
 
-matriz = [["X", 2, 3],
-          [1, 4, 5],
-          [8, 7, 6]]
+# Tem solução
+# matriz = [[1, 8, 2],
+#           ["X", 4, 3],
+#           [7, 6, 5]]
+matriz = [[5, 2, 8],
+          [4, 1, 7],
+          ["X", 3, 6]]
+
+
+
+# Não tem solução
+# matriz = [[8, 1, 2],
+#           ["X", 4, 3],
+#           [7, 6, 5]]
+# matriz = [[1, 2, 3],
+#           [4, 5, 6],
+#           ["X", 8, 7]]
 
 # [[(0, 0), (1, 0), (2, 0)],
 #  [(0, 1), (1, 1), (2, 1)],
@@ -18,11 +33,13 @@ lista_abertos.append(nodo_origem)
 # for i in range(4):
 achou_objetivo = False
 while not achou_objetivo:
+# for i in range(10):
     nodo_atual: Nodo = lista_abertos[0]
-    novos_nodos: list = nodo_atual.gerar_filhos()
 
     print(">>> Nodo pai:")
     nodo_atual.print_estado()
+
+    novos_nodos: list = nodo_atual.gerar_filhos()
 
     lista_visitados.append(nodo_atual)
     lista_abertos.remove(nodo_atual)
@@ -42,6 +59,9 @@ while not achou_objetivo:
     #     print(nodo.estado)
 
     achou_objetivo = nodo_atual.esta_completo()
+    if achou_objetivo == True:
+        print("sucesso!")
+        print(nodo_atual.estado)
 
-print("sucesso!")
-print(lista_abertos[0].estado)
+# print("sucesso!")
+# print(lista_visitados[0].estado)
